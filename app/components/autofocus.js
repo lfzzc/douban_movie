6282ad4cab9c66fd502c8ab478c8c694356af5ba
lfzzc <408605867@qq.com>
2016-11-09 20:54:54 +0800

    '修改autofixbBug'

--- app/components/autofocus.js
+++ app/components/autofocus.js
@@ -1,22 +1,17 @@
-/**
- * Created by Administrator on 2016/11/9.
- */
 (function (angular) {
 	angular.module('movie.directive.auto',[])
 		.directive('autoFocus',['$location',function ($location) {
-			var path=$location.path();
 			return {
 				restrict:'A',
 				link:function ($scope, iElm, iAttrs, controller) {
-					var aLink=iElm.children().attr('href');
-					console.log(aLink);
-					var type=aLink.replace(/#(\/.+?)\/\d/,'$1');
-					if(path.startsWith(type)){
-						iElm.addClass('active')
-					}
-					iElm.on('click',function () {
-						iElm.parent().children().removeClass('active');
-						iElm.addClass('active')
+					$scope.$location=$location;
+					$scope.$watch('$location.path()',function (now) {
+						var aLink=iElm.children().attr('href');
+						var type=aLink.replace(/#(\/.+?)\/\d/,'$1');
+						if(now.startsWith(type)){
+							iElm.parent().children().removeClass('active');
+							iElm.addClass('active')
+						}
 					});
 				}
 			}
